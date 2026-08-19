0678c81ce51421e84135b42562e6e3bc82b8038e
KulyaevAlex <kavrzn86@gmial.com>
2026-08-19 13:03:57 +0300

    add class Main

---
+++ src/main/Main.java
@@ -0,0 +1,9 @@
+package src.main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Алексей");
+        System.out.println("39 лет");
+        System.out.println("Java");
+    }
+}
